d6488f68a4bf9194c5d93e4b9f5e1174c9709ee1
Mikhail-hud <mykhaylohud@gmail.com>
2021-03-08 15:23:20 +0200

    object

--- ch05/operations.js
+++ ch05/operations.js
@@ -9,11 +9,32 @@ if (username) {
 } // только если username не равно !== Null или undefined;
 
 let n = 4;
+if (n === 1) {
+  console.log(n);
+} else if (n === 2) {
+  console.log(n);
+} else if (n === 3) {
+  console.log(n);
+} else if (n === 4) {
+  console.log(4); // выполняеть если n === 4,
+} else {
+  console.log(n); // если не одна из условий не подошло, тогда выполняеться else;
+}
+
+for (let i = 0; i < data.length; i++) {
+  if (!data[i]) continue; // оператор continue перзапускает цикл со следующей итерацией
+  // не удаеться продолжить с неопределенными данными.
+  total += data[i];
+}
+
+for (let i = 0; i < a.length; i++) {
+  if (a[i] === target) break; // преждевременный выход из цикла.
+}
 
 switch (n) {
   case 6:
     console.log(n);
-    break;
+    break; // преривыет оператор switch и выход из него.
   case 7:
     console.log(n);
     break;
@@ -26,4 +47,95 @@ let count = 0;
 while (count < 10) {
   console.log(count);
   count++;
-}// from 0 to 9
+} // from 0 to 9
+
+let i;
+let j;
+let sum = 0;
+for (i = 0, j = 10; i <= 10; i++, j--) {
+  sum += i + j;
+} // 110
+
+function tail(o) {
+  for (; o.next; o = o.mext) {
+    return o;
+  }
+}
+
+let data = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+let sum = 0;
+for (const item of data) {
+  sum += item;
+} // 45  работатет с итерируемыми обьектами. По умолчанию обьекты не являються итерируемыми. С обыкновенными обьектами приводит к генерированию ошибки Type Error;
+
+let o = {
+  x: 1,
+  y: 2,
+  z: 3,
+};
+
+for (const item of o) {
+  console.log(item); // Uncaught TypeError: o is not iterable
+}
+
+for (const item of Object.keys(o)) {
+  console.log(item); // x,y,z для получения свойст обьекта
+}
+
+for (const item of Object.values(o)) {
+  console.log(item); // 1,2,3 , проход по значениям
+}
+
+for (const [k, i] of Object.entries(o)) {
+  console.log(k, i); // x 1, y 2, z 3;. для получения свойст и значения через деструктурирущие присваивание
+}
+
+let frequency = {};
+for (const letter of 'mississippi') {
+  if (frequency[letter]) {
+    frequency[letter]++;
+  } else {
+    frequency[letter] = 1;
+  }
+} //  {m: 1, i: 4, s: 4, p: 2}
+
+let m = new Map([[1, 'one']]);
+for (const [k, i] of m) {
+  console.log(k, i); // 1, one;
+}
+
+let text = 'Na na na na na na na na Batman!';
+let wordSet = new Set(text.split(' '));
+let uniqe = [];
+
+for (const word of wordSet) {
+  uniqe.push(word); // ["Na", "na", "Batman!"]
+}
+
+async function print(stream) {
+  for await (let chunk of stream) {
+    console.log(chunk); // асинхронный атератор
+  }
+}
+
+let o = {
+  x: 1,
+  y: 2,
+  z: 3,
+};
+for (const item in o) {
+  console.log(o[item]); // 1,2,3. значение каждого свойства
+}
+
+throw // результатом вычисления оператора может быть значение любоого типа, можна указать число, строку..
+
+try {
+  // в нормальной ситуации этот код выполняеться от ночала до конца. Но может генерировать исключение либо напрямую с помощью оператора throw, либо коственно.
+  
+} catch (error) {
+  // операторы в данном блоке выполняються, если и только если в блоке try было сгенерировано исключение
+  
+} finally {
+  // операторы в данном блоке выполняються, независимо от того, что произошло в блоке try 
+
+}
--- ch06/merge.js
+++ ch06/merge.js
@@ -1,14 +1,81 @@
 // Like Object.assign() but doesn't override existing properties
 // (and also doesn't handle Symbol properties)
 function merge(target, ...sources) {
-    for(let source of sources) {
-        for(let key of Object.keys(source)) {
-            if (!(key in target)) { // This is different than Object.assign()
-                target[key] = source[key];
-            }
-        }
+  for (let source of sources) {
+    for (let key of Object.keys(source)) {
+      if (!(key in target)) {
+        // This is different than Object.assign()
+        target[key] = source[key];
+      }
     }
-    return target;
+  }
+  return target;
 }
-Object.assign({x: 1}, {x: 2, y: 2}, {y: 3, z: 4})  // => {x: 2, y: 3, z: 4}
-merge({x: 1}, {x: 2, y: 2}, {y: 3, z: 4})          // => {x: 1, y: 2, z: 4}
+Object.assign({ x: 1 }, { x: 2, y: 2 }, { y: 3, z: 4 }); // => {x: 2, y: 3, z: 4}
+merge({ x: 1 }, { x: 2, y: 2 }, { y: 3, z: 4 }); // => {x: 1, y: 2, z: 4}
+
+book.edition = 7; // создание свойства обэкта book
+book['main title'] = 'ECMAScript'; // изменение свойства
+
+//Наследование
+
+let unitcircle = { r: 1 };
+let c = Object.create(unitcircle); // c наследует свойство r
+c.x = 1;
+c.y = 2;
+c.r = 2;
+unitcircle.r; // 1: прототип не затронут.
+
+let o = { x: 1 };
+'x' in o; // опертация in возвращает true если обьект имеет собственное или унаследованное свойство
+
+o.hasOwnProperty('x'); //опертация  возвращает true если обьект имеет собственное свойство
+Object.keys(o); //  возвращает масив имён перечислимых собственных свойст обьекта. Не влючает унаследованные свойства.
+Object.getOwnPropertyNames(o); //  возвращает также  масив имён не перечислимых собственных свойст обьекта. ПРи условии, что имена предоставленные строками
+
+//Разширение обьектов
+let target = { x: 1 };
+let source = { y: 2, z: 3 };
+for (const key of Object.keys(source)) {
+  target[key] = source[key];
+}
+target; // {x:1, y:2, z:3}
+
+Object.assign(); // ожидает получения в своих аргументах двух и болие обьектов. Она модифицирует и возвращает первый аргумент, в котором указан целевой обьекты
+
+//Операция распространения
+
+let position = { x: 0, y: 0 };
+let dimensions = { width: 100, height: 75 };
+
+let rect = { ...position, ...dimensions };
+rect.x + rect.y + rect.width + rect.height; // 175
+
+let o = { x: 1 };
+let p = { x: 0, ...o };
+p.x; // 1 значение из обьекта O переопределяет начальное значение
+
+let q = { ...o, x: 2 };
+q.x; // 2 значение 2 переопеределяет предыдущиее значение из о
+
+// Вычисляемые свойства
+const PROPERTY_NAME = 'p1';
+function computePropertyName() {
+  return 'p' + 2;
+}
+let p = {
+  [PROPERTY_NAME]: 1,
+  [computePropertyName()]: 2,
+};
+p.p1 + p.p2; // 3
+
+// сокращённая щапись методов
+
+let square = {
+  area() {
+    return this.side;
+  },
+  side: 10,
+};
+
+square.area();
