ab89bfd6e0f76a6bbdde09d9a8bc9ff80db0f268
Mikhail-hud <mykhaylohud@gmail.com>
2021-03-27 21:18:07 +0200

    Class

--- ch08/function.js
+++ ch08/function.js
@@ -1,7 +1,11 @@
-const square = function (x) {return x * x}
+const square = function (x) {
+  return x * x
+}
 square(4) // 16
 
-const sum = (x, y) => {return x + y};
+const sum = (x, y) => {
+  return x + y
+};
 sum(10, 5) // 15
 
 const sum1 = (x, y) => x + y;
@@ -10,7 +14,9 @@ sum1(10, 15); // 25
 const sum3 = x => x + 25;
 sum3(5); // 30
 
-const f = x => {return {value: x}}
+const f = x => {
+  return {value: x}
+}
 f(5) // {value: 5} return object
 
 const g = x => ({value: x})
@@ -18,7 +24,7 @@ g(5)// {value:5} return object
 
 let filtered = [1, null, 3, 4].filter(x => x !== null) // [1,3,4]
 
-let squares = [1,2,3,4,5].map(x => x * 2) //  [2, 4, 6, 8, 10]
+let squares = [1, 2, 3, 4, 5].map(x => x * 2) //  [2, 4, 6, 8, 10]
 
 // Вложенные функции
 
@@ -26,77 +32,170 @@ function test(a, b) {
   function f1(x) {
     return x + 2
   }
+
   return (f1(a) + f1(b))
 }
+
 test(2, 2) // 8
 
 let o = {}
-o.m = function (x) {return x + 1};
+o.m = function (x) {
+  return x + 1
+};
 o.m(5) // 6
 
 
-const rectangle = (width, height = width * 2) =>  ({width, height})
+const rectangle = (width, height = width * 2) => ({width, height})
 rectangle(25); // {width: 25, height: 50}
 
-function max (first, ...rest) {
+function max(first, ...rest) {
   let maxValue = first
   for (let n of rest) {
-    if( n > maxValue) {
+    if (n > maxValue) {
       maxValue = n
     }
   }
   return maxValue
 }
+
 max(1, 2, 50, 150, 2500, 400);  // 2500
 
-function max1 (item) {
+function max1(item) {
   let maxValue = 0;
   for (let i = 0; i < arguments.length; i++) {
     if (arguments[i] > maxValue) maxValue = arguments[i]
   }
   return maxValue
 }
+
 max1(1, 2, 4, 50, 25)// 50 обьэкт arguments [1, 2, 4, 50, 25, callee: ƒ, Symbol(Symbol.iterator): ƒ] стоит избегать,
 // заменить его ...args
 
-function max2 (...item) {
+function max2(...item) {
   let maxValue = 0;
   for (let i = 0; i < item.length; i++) {
     if (item[i] > maxValue) maxValue = item[i]
   }
   return maxValue
 }
+
 max2(55, 2, 4, 50, 25) //55
 
 /// Деструктуризация аргументов
- function add(x, y) {
-  return [x[0] + y[0], x[1] + y[1] ]
- }
- add([1,2], [3,5]) // [4,7]
+function add(x, y) {
+  return [x[0] + y[0], x[1] + y[1]]
+}
 
-function add2 ([x, y], [x1, y1]) {
+add([1, 2], [3, 5]) // [4,7]
+
+function add2([x, y], [x1, y1]) {
   return [x + x1, y + y1]
 }
-add2([1,2], [3,5]) // [4,7]
 
-function add3({w,z}, scalar) {
-  return {x: w + scalar, y: z + scalar }
+add2([1, 2], [3, 5]) // [4,7]
+
+function add3({w, z}, scalar) {
+  return {x: w + scalar, y: z + scalar}
 }
-add3({w:1, z:2}, 4) // {x: 5, y: 6}
 
-function add4({x:x1, y: y1}, {z:z1, w: w1}) {
+add3({w: 1, z: 2}, 4) // {x: 5, y: 6}
+
+function add4({x: x1, y: y1}, {z: z1, w: w1}) {
   return {x: x1 + y1, y: z1 + w1}
 }
-add4({x:1, y:1}, {z:2, w:3}) // {x: 2, y: 5}
 
-function unit(x) {return x * x}
+add4({x: 1, y: 1}, {z: 2, w: 3}) // {x: 2, y: 5}
+
+function unit(x) {
+  return x * x
+}
+
 let s = unit
 s(16) // 256
-unit(16 ) // 256
+unit(16) // 256
 
-let unit2 = { test: function (x) {return x * x}}
+let unit2 = {
+  test: function (x) {
+    return x * x
+  }
+}
 
 unit2.test(16) // 256
 
 let a = [x => x * x, 45]
 a[0](5)// 25
+
+// Замыкание
+
+function makeCounter(initialValue) {
+  return function (increase) {
+    return initialValue += initialValue + increase;
+  };
+}
+
+let counter1 = makeCounter(0);
+let counter2 = makeCounter(0);
+
+console.log(counter1(2)); // 2
+console.log(counter1(2)); // 6
+
+console.log(counter1(2)) // 14
+console.log(counter2(2)); // 2
+
+
+function counter(n) {
+  return {
+    count() {
+      return n++
+    },
+    reset() {
+      return n = 0
+    }
+  }
+}
+
+let c = counter(0);
+let d = counter(0);
+console.log(c.count()) // 0
+console.log(c.count()) // 1
+console.log(c.reset()) // 0
+console.log(c.count()) // 0
+console.log(c.count()) // 1
+
+/// Функции высшего порядка ― это такие функции, которые принимают функцию в качестве аргумента и возвращают функцию в качестве вывода.
+
+const arr1 = [1, 2, 3];
+const arr2 = arr1.map(item => item * 2);
+
+const persons = [
+  {name: 'Peter', age: 16},
+  {name: 'Mark', age: 18},
+  {name: 'John', age: 27},
+  {name: 'Jane', age: 14},
+  {name: 'Tony', age: 24},
+];
+const fullAge = persons.filter(person => person.age >= 18);
+
+const arr = [5, 7, 1, 8, 4];
+const sum = arr.reduce(function (accumulator, currentValue) {
+  return accumulator + currentValue;
+}, 10); // prints 35
+
+
+const strArray = ['JavaScript', 'Python', 'PHP', 'Java', 'C'];
+
+function mapForEach(arr, fn) {
+  const newArray = [];
+  for (let i = 0; i < arr.length; i++) {
+    newArray.push(
+      fn(arr[i])
+    );
+  }
+  return newArray;
+}
+
+const lenArray = mapForEach(strArray, function (item) {
+  return item.length;
+});
+// prints [ 10, 6, 3, 4, 1 ]
+console.log(lenArray);
--- ch09/Complex.js
+++ ch09/Complex.js
@@ -4,61 +4,200 @@
  * imaginary number and that the imaginary number i is the square root of -1.
  */
 class Complex {
-    // Once class field declarations are standardized, we could declare
-    // private fields to hold the real and imaginary parts of a complex number
-    // here, with code like this:
-    //
-    // #r = 0;
-    // #i = 0;
+  // Once class field declarations are standardized, we could declare
+  // private fields to hold the real and imaginary parts of a complex number
+  // here, with code like this:
+  //
+  // #r = 0;
+  // #i = 0;
 
-    // This constructor function defines the instance fields r and i on every
-    // instance it creates. These fields hold the real and imaginary parts of
-    // the complex number: they are the state of the object.
-    constructor(real, imaginary) {
-        this.r = real;       // This field holds the real part of the number.
-        this.i = imaginary;  // This field holds the imaginary part.
-    }
+  // This constructor function defines the instance fields r and i on every
+  // instance it creates. These fields hold the real and imaginary parts of
+  // the complex number: they are the state of the object.
+  constructor(real, imaginary) {
+    this.r = real;       // This field holds the real part of the number.
+    this.i = imaginary;  // This field holds the imaginary part.
+  }
 
-    // Here are two instance methods for addition and multiplication
-    // of complex numbers. If c and d are instances of this class, we
-    // might write c.plus(d) or d.times(c)
-    plus(that) {
-        return new Complex(this.r + that.r, this.i + that.i);
-    }
-    times(that) {
-        return new Complex(this.r * that.r - this.i * that.i,
-                           this.r * that.i + this.i * that.r);
-    }
+  // Here are two instance methods for addition and multiplication
+  // of complex numbers. If c and d are instances of this class, we
+  // might write c.plus(d) or d.times(c)
+  plus(that) {
+    return new Complex(this.r + that.r, this.i + that.i);
+  }
 
-    // And here are static variants of the complex arithmetic methods.
-    // We could write Complex.sum(c,d) and Complex.product(c,d)
-    static sum(c, d) { return c.plus(d); }
-    static product(c, d) { return c.times(d); }
+  times(that) {
+    return new Complex(this.r * that.r - this.i * that.i,
+      this.r * that.i + this.i * that.r);
+  }
 
-    // These are some instance methods that are defined as getters
-    // so they're used like fields. The real and imaginary getters would
-    // be useful if we were using private fields this.#r and this.#i
-    get real() { return this.r; }
-    get imaginary() { return this.i; }
-    get magnitude() { return Math.hypot(this.r, this.i); }
+  // And here are static variants of the complex arithmetic methods.
+  // We could write Complex.sum(c,d) and Complex.product(c,d)
+  static sum(c, d) {
+    return c.plus(d);
+  }
 
-    // Classes should almost always have a toString() method
-    toString() { return `{${this.r},${this.i}}`; }
+  static product(c, d) {
+    return c.times(d);
+  }
 
-    // It is often useful to define a method for testing whether
-    // two instances of your class represent the same value
-    equals(that) {
-        return that instanceof Complex &&
-            this.r === that.r &&
-            this.i === that.i;
-    }
+  // These are some instance methods that are defined as getters
+  // so they're used like fields. The real and imaginary getters would
+  // be useful if we were using private fields this.#r and this.#i
+  get real() {
+    return this.r;
+  }
 
-    // Once static fields are supported inside class bodies, we could
-    // define a useful Complex.ZERO constant like this:
-    // static ZERO = new Complex(0,0);
+  get imaginary() {
+    return this.i;
+  }
+
+  get magnitude() {
+    return Math.hypot(this.r, this.i);
+  }
+
+  // Classes should almost always have a toString() method
+  toString() {
+    return `{${this.r},${this.i}}`;
+  }
+
+  // It is often useful to define a method for testing whether
+  // two instances of your class represent the same value
+  equals(that) {
+    return that instanceof Complex &&
+      this.r === that.r &&
+      this.i === that.i;
+  }
+
+  // Once static fields are supported inside class bodies, we could
+  // define a useful Complex.ZERO constant like this:
+  // static ZERO = new Complex(0,0);
 }
 
 // Here are some class fields that hold useful predefined complex numbers.
-Complex.ZERO = new Complex(0,0);
-Complex.ONE = new Complex(1,0);
-Complex.I = new Complex(0,1);
+Complex.ZERO = new Complex(0, 0);
+Complex.ONE = new Complex(1, 0);
+Complex.I = new Complex(0, 1);
+
+
+// Es 5
+function Rang(from, to) {
+  this.from = from
+  this.to = to
+
+}
+
+Rang.prototype = {
+  includes: function (x) {
+    return this.from <= x && x <= this.to
+  },
+  toString: function () {
+    return "(" + this.from + " " + this.to + ")"
+  },
+  [Symbol.iterator]: function* () {
+    for (let x = Math.ceil(this.from); x <= this.to; x++) yield  x;
+  }
+}
+
+let r = new Rang(1,3) // => {from: 1, to: 3}
+
+r.includes(2) // => true;
+r.toString() // => (1 3);
+let test = [...r] // [1, 2, 3]
+
+class Animal {
+  static type = 'Animal' // доступно только у класса Animal.type => "Animal"
+
+  constructor(options) {
+    this.name = options.name
+    this.age = options.age
+    this.hasTail = options.hasTail
+  }
+
+  voice() {
+    console.log(`Hello, i am ${this.name}`)
+  }
+}
+
+// console.log(typeof(Animal)) // Function
+// console.log(typeof(Animal.prototype)) // Object
+// console.log(Animal.prototype) //
+/*{constructor: ƒ, voice: ƒ}
+constructor: class Animal
+voice: ƒ voice()
+///-------------------///
+В JavaScript функция может использоваться как конструктор.
+Это означает, что мы можем создавать из них объекты, используя ключевое слово new.
+Каждая функция-конструктор имеет встроенный объект, связанный с ними. Этот встроенный объект называется прототипом.
+Экземпляры функции-конструктора используют __proto__ для доступа к свойству прототипа своей функции-конструктора/класса.
+
+JavaScript часто описывают как язык прототипного наследования — каждый объект, имеет объект-прототип, который выступает как шаблон, от которого объект наследует методы и свойства.
+Объект-прототип так же может иметь свой прототип и наследовать его свойства и методы и так далее.
+Это часто называется цепочкой прототипов и объясняет почему одним объектам доступны свойства и методы которые определены в других объектах.
+
+Точнее, свойства и методы определяются в свойстве prototype функции-конструктора объектов, а не в самих объектах
+__proto__:
+  constructor: ƒ Object()
+hasOwnProperty: ƒ hasOwnProperty()
+isPrototypeOf: ƒ isPrototypeOf()
+propertyIsEnumerable: ƒ propertyIsEnumerable()
+toLocaleString: ƒ toLocaleString()
+toString: ƒ toString()
+valueOf: ƒ valueOf()
+__defineGetter__: ƒ __defineGetter__()
+__defineSetter__: ƒ __defineSetter__()
+__lookupGetter__: ƒ __lookupGetter__()
+__lookupSetter__: ƒ __lookupSetter__()
+get __proto__: ƒ __proto__()
+set __proto__: ƒ __proto__()*/
+
+const animal = new Animal({name: 'Cat', age: 5, hasTail: true})
+
+console.log(animal.__proto__ === Animal.prototype); // true
+
+class Cat extends Animal {
+  static type = 'Cat'
+
+  constructor(options) {
+    super(options);
+    this.color = options.color
+  }
+
+  voice() {
+    super.voice(); // Hello, i am Cat
+    console.log('Hello, i have lost voice'); // Hello, i have lost voice
+  }
+
+  get ageInfo() {
+    return this.age * 5
+  } // cat.ageInfo => 10
+
+  set ageInfo(newAge) {
+    return this.age = newAge
+  }//  cat.ageInfo = 8 => 8
+}
+
+const cat = new Cat({name: "Cat", age: '2', hasTail: true, color: 'black'})
+
+
+let data = {
+  name: 'misha',
+  age: 25,
+  getData() {
+    return `Hello, my name is ${this.name} and i am ${this.age} years old `
+  }
+}
+
+let copy = Object.create(data) // создаёт новый объект из указанного объекта-прототипа.
+
+copy.getData = function () {
+  return 'Copy'
+}
+console.log(copy.getData()) // Copy
+console.log(data.getData()); //  Hello, my name is misha and i am 25 years old
+
+Object.prototype.hello = function () {
+  alert("Hello")
+}
+
+copy.hello() // Alert with text 'Hello'
